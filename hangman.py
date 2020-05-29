43ab2dfd14cab1361b883fe97c2080bdc670e494
Anmol Kalra <42032383+anmolkalra17@users.noreply.github.com>
2020-05-29 16:23:03 +0530

    final release

--- hangman.py
+++ hangman.py
@@ -1,49 +1,61 @@
 # Write your code here
 import random
 
-print("H A N G M A N\n")
-words = ['python', 'java', 'kotlin', 'javascript']
-word = random.choice(words)
 
-out = len(word) * "-"
-guess_list = []
-garbage = []
-life = 1
-print(out)
-while True:
-    print(len(guess_list), len(garbage))
-    guess_letter = input("Input a letter: ")
+def play():
+    words = ['python', 'java', 'kotlin', 'javascript']
+    word = random.choice(words)
 
-    if guess_letter in word:
-        guess_list.append(guess_letter)
-    else:
-        garbage.append(guess_letter)
-
-    if len(guess_letter) != 1 or len(guess_letter) is None:
-        print("You should input a single letter")
-
-    elif guess_letter.isupper() or guess_letter.isnumeric() or (not guess_letter.isalpha()):
-        print("It is not an ASCII lowercase letter")
-
-    elif guess_list.count(guess_letter) > 1 or garbage.count(guess_letter) > 1:
-        print("You already typed this letter ")
-
-    elif guess_letter not in word and guess_letter.isalpha() and guess_letter.islower():
-        print("No such letter in the word")
-        life += 1
-
-    for i in range(0, len(word)):
-        if guess_letter == word[i]:
-            out = out[:i] + guess_letter + out[i + 1:]
-
-    if out == word:
-        print('You guessed the word!')
-        print('You survived!')
-        exit()
-
-    if life == 9:
-        print("You are hanged!")
-        exit()
-
-    print()
+    out = len(word) * "-"
+    guess_list = []
+    garbage = []
+    life = 1
     print(out)
+    while True:
+        guess_letter = input("Input a letter: ")
+
+        if guess_letter in word:
+            guess_list.append(guess_letter)
+        else:
+            garbage.append(guess_letter)
+
+        if len(guess_letter) != 1 or len(guess_letter) is None:
+            print("You should input a single letter")
+
+        elif guess_letter.isupper() or guess_letter.isnumeric() or (not guess_letter.isalpha()):
+            print("It is not an ASCII lowercase letter")
+
+        elif guess_list.count(guess_letter) > 1 or garbage.count(guess_letter) > 1:
+            print("You already typed this letter ")
+
+        elif guess_letter not in word and guess_letter.isalpha() and guess_letter.islower():
+            print("No such letter in the word")
+            life += 1
+
+        for i in range(0, len(word)):
+            if guess_letter == word[i]:
+                out = out[:i] + guess_letter + out[i + 1:]
+
+        if out == word:
+            print('You guessed the word!')
+            print('You survived!')
+            exit()
+
+        if life == 9:
+            print("You are hanged!")
+            exit()
+
+        print()
+        print(out)
+
+
+def menu():
+    print("H A N G M A N\n")
+    choice = input('Type "play" to play the game, "exit" to quit:')
+    if choice == "play":
+        play()
+    elif choice == "quit":
+        exit()
+
+
+menu()
