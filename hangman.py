349a35339eeeb84f0d831250ba80638ff2ff7fda
Anmol Kalra <42032383+anmolkalra17@users.noreply.github.com>
2020-05-29 15:32:37 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -20,18 +20,15 @@ while win is False:
 
     if len(guess_letter) != 1 or guess_letter is None:
         print("You should input a single letter")
-        life += 1
 
     if guess_letter.isupper() or guess_letter in special_char or guess_letter.isnumeric() or guess_letter is None:
         print("It is not an ASCII lowercase letter")
-        life += 1
 
     if guess_letter not in word:
         not_needed.append(guess_letter)
 
     if guess_list.count(guess_letter) > 1 or not_needed.count(guess_letter) > 1:
         print("You already typed this letter")
-        life += 1
 
     elif guess_letter not in word and guess_letter.islower() and guess_letter not in special_char:
         print("No such letter in the word")
@@ -48,7 +45,6 @@ while win is False:
         exit()
 
     if life == 8:
-        print("No such letter in the word")
         print("You are hanged!")
         exit()
 
