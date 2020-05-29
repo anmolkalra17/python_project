17737da1de6b210ebcfa9b82869e4d1950aee46a
Anmol Kalra <42032383+anmolkalra17@users.noreply.github.com>
2020-05-29 14:48:27 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -10,42 +10,45 @@ guess_list = []
 not_needed = []
 special_char = '+-/*!@#$%^&()_-=[]{}:;"\'<>.?|\`~ '
 life = 0
+win = False
 print(out)
-while True:
+while win is False:
     guess_letter = input("Input a letter: ")
 
     if guess_letter in word:
         guess_list.append(guess_letter)
 
-    if guess_letter.isupper() or guess_letter in special_char or guess_letter.isnumeric() or guess_letter is None:
-        print("It is not an ASCII lowercase letter")
-        life += 1
-
     if len(guess_letter) != 1 or guess_letter is None:
         print("You should input a single letter")
+        life += 1
+
+    if guess_letter.isupper() or guess_letter in special_char or guess_letter.isnumeric() or guess_letter is None:
+        print("It is not an ASCII lowercase letter")
         life += 1
 
     if guess_letter not in word:
         not_needed.append(guess_letter)
 
-    if guess_letter not in word and guess_letter.islower() and guess_letter not in special_char:
-        print("No such letter in the word")
+    if guess_list.count(guess_letter) > 1 or not_needed.count(guess_letter) > 1:
+        print("You already typed this letter")
         life += 1
 
-    if guess_list.count(guess_letter) > 1 or not_needed.count(guess_letter) > 1:
+    elif guess_letter not in word and guess_letter.islower() and guess_letter not in special_char:
+        print("No such letter in the word")
         life += 1
-        print("You already typed this letter")
 
     for i in range(0, len(word)):
         if guess_letter == word[i]:
             out = out[:i] + guess_letter + out[i + 1:]
 
     if out == word:
+        win = True
         print('You guessed the word!')
         print('You survived!')
         exit()
 
     if life == 8:
+        print("No such letter in the word")
         print("You are hanged!")
         exit()
 
